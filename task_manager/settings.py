eacd08700054a13dea1d4f69981876d8a5f1dace
Regina Khairutdinova <regina.khajrutdinova@gmail.com>
2025-04-03 23:31:49 +0400

    update settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -8,20 +8,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
-
 DEBUG = os.getenv("DEBUG", "False") == "True"
-
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
+
+ROOT_URLCONF = 'task_manager.urls'
+WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600)
 }
 
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-ROOT_URLCONF = 'task_manager.urls'
-
-WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 INSTALLED_APPS = [
     'task_manager',
