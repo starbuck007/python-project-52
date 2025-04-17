a55f1812b4a1bd289bea4c9f566762d497d43b2c
Regina Khairutdinova <regina.khajrutdinova@gmail.com>
2025-04-17 21:08:19 +0400

    add routes for users

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -24,17 +24,48 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 INSTALLED_APPS = [
     'task_manager',
     'django_bootstrap5',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
 ]
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR / 'task_manager' / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
     },
 ]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
+
+LOGIN_URL = 'login'
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,8 +1,16 @@
+from django.contrib import admin
 from django.urls import path
-from django.shortcuts import render
+from .views import (index_view, UserListView, UserCreateView, UserUpdateView,
+                    UserDeleteView, login_view, logout_view)
 
 
 urlpatterns = [
-    path('', lambda request: render(request,
-                                    'index.html'), name='home'),
+    path('admin/', admin.site.urls),
+    path('', index_view, name='home'),
+    path('users/', UserListView.as_view(), name='user_list'),
+    path('users/create/', UserCreateView.as_view(), name='user_create'),
+    path('users/<int:pk>/update/', UserUpdateView.as_view(), name='user_update'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('users/<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete'),
 ]
